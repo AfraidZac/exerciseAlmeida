84c7a403810791089af106246e4aab734a1484ef
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-06-22 17:13:14 +0100

    Database connection and Registration
    database is fully functional and ready to go, registration is still in
    development with authorizations to complete and requirements to finish.

--- Project/public_html/form.php
+++ Project/public_html/form.php
@@ -13,21 +13,27 @@ echo "
     <head>
         <title>Register</title>
         <center><h1>Register Form</h1></center>
+        <script src = '/js/scripts.js/' ></script>
     </head>
     <body>
-    <li>Email *<input type='text' name='email' id='email'placeholder='joe@example.com'/></li><br>
-    <li>Email confirmation *<input type='text' name='emailcheck' id='emailcheck'/></li><br>
-    <li>Password *<input type='password' name='password' id='passoword'/></li><br>
-    <li>Password confirmation *<input type='password' name='passwordcheck' id='passwordcheck'/></li><br>
-    <li>Name *<input type='text' name='firstname' id='firstname' placeholder='First name'/><input type='text' name='lastname' id='lastname' placeholder='Last name'/></li><br>
-    <li>Address *<input type='text' name='address' id='address'/></li><br>
-    <li>Postal *<input type='text' name='postal' id='postal' placeholder='xxxx-xxx'/> <input type='text' name='locality' id='locality' placeholder='Mafra'/></li><br>
+    <form name='form' action='submit.php' method='post'>
+        <li>Email *<input type='text' name='email' id='email'placeholder='joe@example.com'/></li><br>
+        <li>Email confirmation *<input type='text' name='emailcheck' id='emailcheck'/></li><br>
+        <li>Password *<input type='password' name='password' id='passoword' onkeyup='return passtest()'/></li><br>
+        <table id=\"show\"></table><br>
+        <li>Password confirmation *<input type='password' name='passwordcheck' id='passwordcheck'/></li><br>
+        <li>Name *<input type='text' name='firstname' id='firstname' placeholder='First name'/><input type='text' name='lastname' id='lastname' placeholder='Last name'/></li><br>
+        <li>Address <input type='text' name='address' id='address'/></li><br>
+        <li>Postal <input type='text' name='postal' id='postal' placeholder='xxxx-xxx'/> <input type='text' name='locality' id='locality' placeholder='Mafra'/></li><br>
     "?>
 <?php
 include 'country.php';
 echo"
-        <li>TIN *<input type='text' name='TIN' id='TIN'/></li><br>
-        <li>Phone *<input type='text' name='phone' id='phone'/></li><br>
+        <li>TIN <input type='number' name='TIN' id='TIN'/></li><br>
+        <li>Phone <input type='number' name='phone' id='phone'/></li><br>
+        <button type='submit' >Complete</button>
+    </form>
+    
     </body>
 </html>
 
---
+++ Project/public_html/js/scripts.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Administrator on 22/06/2017.
+ */
+function requiredfields() {
+    var pass = document.forms["form"]["password"].value;
+    var passcheck= document.forms["form"]["passwordcheck"].value;
+    var email= document.forms["form"]["email"].value;
+    var emailcheck= document.forms["form"]["emailcheck"].value;
+    var name= document.forms["form"]["name"].value;
+
+}
+function passtest(){
+    var code = document.getElementById("password").value;
+    var strength = 0;
+    var show = document.getElementById("show");
+    if((code.length >= 4) && (code.length <= 7)){
+        strength += 10;
+    }else if(code.length>7){
+        strength += 25;
+    }
+    if(code.match(/[a-z]+/)){
+        strength += 10;
+    }
+    if(code.match(/[A-Z]+/)){
+        strength += 20;
+    }
+    if(code.match(/\d+/)){
+        strength += 20;
+    }
+    if(code.match(/\W+/)){
+        strength += 25;
+    }
+    if(strength < 30){
+        show.innerHTML = '<tr><td bgcolor="red" width="'+strength+'"></td><td>Muito Fraca </td></tr>';
+    }else if((strength >= 30) && (strength < 60)){
+        show.innerHTML = '<tr><td bgcolor="yellow" width="'+strength+'"></td><td>Fraca </td></tr>';
+    }else if((strength >= 60) && (strength < 85)){
+        show.innerHTML = '<tr><td bgcolor="blue" width="'+strength+'"></td><td>Decente </td></tr>';
+    }else{
+        show.innerHTML = '<tr><td bgcolor="green" width="'+strength+'"></td><td>Forte </td></tr>';
+    }
+    return;
+}
